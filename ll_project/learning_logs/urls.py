409bc3924facb3e810ee37afce8d18bb497cf688
chuliu-hash <3121607533@qq.com>
2025-05-07 16:58:12 +0800

    modified:   ll_project/db.sqlite3 	modified:   ll_project/learning_logs/__pycache__/urls.cpython-311.pyc 	modified:   ll_project/learning_logs/__pycache__/views.cpython-311.pyc 	new file:   ll_project/learning_logs/templates/learning_logs/edit_entry.html 	new file:   ll_project/learning_logs/templates/learning_logs/edit_topic.html 	modified:   ll_project/learning_logs/templates/learning_logs/index.html 	modified:   ll_project/learning_logs/templates/learning_logs/topic.html 	modified:   ll_project/learning_logs/urls.py 	modified:   ll_project/learning_logs/views.py

--- ll_project/learning_logs/urls.py
+++ ll_project/learning_logs/urls.py
@@ -8,12 +8,22 @@ app_name = 'learning_logs'
 urlpatterns = [
 	# 主页, http://localhost:8000/
 	path('', views.index, name='index'),
+
 	# 主题列表页, http://localhost:8000/topics/
 	path('topics/', views.topics, name='topics'),
+
 	# 主题详情页, http://localhost:8000/topics/topic_id/
 	path('topics/<int:topic_id>/', views.topic, name='topic'),
+
 	# 添加新主题, http://localhost:8000/new_topic/
 	path('new_topic/', views.new_topic, name='new_topic'),
+
 	# 在特定主题添加新条目, http://localhost:8000/new_entry/topic_id/
 	path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+
+	# 编辑主题, http://localhost:8000/edit_topic/topic_id/
+	path('edit_topic/<int:topic_id>/', views.edit_topic, name='edit_topic'),
+	
+	# 编辑特定条目， http://localhost:8000/edit_entry/entry_id/
+	path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
--- ll_project/learning_logs/views.py
+++ ll_project/learning_logs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 # Create your views here.
 
@@ -49,6 +49,41 @@ def new_entry(request, topic_id):
 			new_entry.topic = topic  # 将主题与条目关联
 			new_entry.save()  # 保存到数据库
 			return redirect('learning_logs:topic', topic_id=topic_id)
+		
 	# 显示空表单或指出表单数据无效
 	context = {'topic': topic, 'form': form}
 	return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_topic(request, topic_id):
+	"""编辑主题"""
+	topic = Topic.objects.get(id=topic_id)
+	if request.method != 'POST':
+		# 初次请求，使用当前主题填充表单
+		form = TopicForm(instance=topic)
+	else:
+		# 使用当前主题填充表单，并根据POST提交的数据对表单进行修改
+		form = TopicForm(instance=topic, data=request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('learning_logs:topic',topic_id=topic.id)
+		
+	# 显示当前表单或指出表单数据无效
+	context = {'topic': topic, 'form': form}
+	return render(request, 'learning_logs/edit_topic.html', context)
+
+def edit_entry(request, entry_id):
+	"""编辑条目"""
+	entry = Entry.objects.get(id=entry_id)
+	topic = entry.topic
+	if request.method != 'POST':
+		# 初次请求，使用当前条目填充表单
+		form = EntryForm(instance=entry)
+	else:
+		# 使用当前条目填充表单，并根据POST提交的数据对表单进行修改
+		form = EntryForm(instance=entry, data=request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('learning_logs:topic', topic_id=topic.id)
+	# 显示当前表单或指出表单数据无效
+	context = {'entry': entry, 'topic': topic, 'form': form}
+	return render(request, 'learning_logs/edit_entry.html', context)
